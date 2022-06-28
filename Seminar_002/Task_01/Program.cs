fae2a9fcb34a768dade2d9e87e97f97d077837a2
igor <igoshinig@yandex.ru>
2022-06-28 18:20:19 +0500

    Задачи 1

---
+++ Seminar_002/Task_01/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, 
+которая выводит случайное трёхзначное число 
+и удаляет вторую цифру этого числа.   456->46*/
+
+int CutNumber()  // метод который возвращает
+{
+    int num = new Random().Next(10,1000);
+    Console.WriteLine("Current random number is " + num);
+    int sotni = num / 100;  // 456 / 100 -> 4 (4.56, где 4 -целое число)
+    int ed = num % 10;      // 456 % 10 -> 6   (45.6, где 6(%) -остаток от деления)
+
+    int result = sotni * 10 + ed;
+    return result;
+}
+
+void CutNumberVoid()
+{
+    int num = new Random().Next(10,1000);
+    Console.WriteLine("Current random number is " + num);
+    int sotni = num / 100;  // 456 / 100 -> 4 (4.56, где 4 -целое число)
+    int ed = num % 10;      // 456 % 10 -> 6   (45.6, где 6(%) -остаток от деления)
+
+    int result = sotni * 10 + ed;
+    Console.WriteLine("Result number is " + result);
+    //Console.WriteLine("Result number is " + number);
+    //return result;      - для int CutNumber метода
+}
+
+CutNumberVoid();
+/*int number = CutNumber();   -для зпуска int CutNumber метода
+Console.WriteLine("Result number is " + number);  -для выдочи результата в терминал int CutNumber метода
+*/
