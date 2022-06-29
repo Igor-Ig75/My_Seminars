a0c73607462c28e3e000e65f2f6f9d583e0f2454
igor <igoshinig@yandex.ru>
2022-06-29 12:53:55 +0500

    Самостоятельная работа 1.

--- Seminar_002/Task_02/Program.cs
+++ Seminar_002/Task_02/Program.cs
@@ -1,2 +1,22 @@
 ﻿// Самостоятельная работа 1.
+/*Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+и показывает наибольшую цифру числа.*/
 
+int NoreNumber()
+{
+    int num = new Random().Next(10,100);
+    Console.WriteLine("Случайное число " + num);
+    int ten = num / 10;
+    int ed = num % 10;
+
+    int max = ten, min = ed;
+
+    if (ed > max) max = ed;
+
+    int result = max;
+    return result;
+
+}
+
+int number = NoreNumber();
+Console.WriteLine("Это наибольшее число " + number);
