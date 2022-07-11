0b8bdfcb4a34347aeec9358f12561aac84252765
igor <igoshinig@yandex.ru>
2022-07-11 18:51:03 +0500

    Напишите программу, которая принимает на вход число (N) и выдаёт квадраты чисел от 1 до N.

--- Seminar_003/Task_03/Program.cs
+++ Seminar_003/Task_03/Program.cs
@@ -2,6 +2,7 @@
 /* Напишите программу, которая принимает на вход число (N) 
 и выдаёт квадраты чисел от 1 до N.
 */
+/*
 void Quad(int x) 
 { 
      int current = 1; 
@@ -12,7 +13,21 @@ void Quad(int x)
         current++; 
      } 
 } 
-
 Console.WriteLine("Введите число: "); 
 int n = Convert.ToInt32(Console.ReadLine()); 
 Quad(n);
+*/
+void Cub(int x) 
+{ 
+     int current = 1; 
+     while (current <= x) 
+     { 
+        int quad = current * current * current; 
+        Console.Write(quad + " "); 
+        current++; 
+     } 
+} 
+
+Console.Write("Введите число: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+Cub(n);
