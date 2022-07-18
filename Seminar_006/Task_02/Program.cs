283403319d31414dd694336ecc28d6e26ac433c9
igor <igoshinig@yandex.ru>
2022-07-18 18:59:17 +0500

    Напишите программу, которая будет преобразовывать десятичное число в двоичное.

---
+++ Seminar_006/Task_02/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+18/2-> 9 0
+ 9/2-> 4 1
+ 4/2-> 2 0
+ 2/2-> 1 0
+ 1/2-> 0 1
+
+18 -> 10010
+*/
+string ChangeDigitSystem(int num)
+{
+    string resultNumder = string.Empty;
+
+    while(num > 0)
+    {
+        resultNumder = num % 2 + resultNumder;
+        num /= 2; // num = num / 2;
+    }
+    return resultNumder;
+}
+
+Console.WriteLine(ChangeDigitSystem(18));
