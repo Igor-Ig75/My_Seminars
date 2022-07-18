f5398ad706919332c3033e386a0fa36809e7560e
igor <igoshinig@yandex.ru>
2022-07-18 19:01:27 +0500

    Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.

---
+++ Seminar_006/Task_04/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: 0 и 1.*/
+
+void PrintArray(int[] array)
+{
+    
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+    Console.WriteLine();
+}
+
+int [] Fibonacci(int n)
+{
+    int[] newArreay = new int[n];
+    Console.WriteLine("Создание массива здесь: ");
+    newArreay[0] = 0;
+    newArreay[1] = 1;
+    for(int i = 2; i < n; i++)
+    {
+        newArreay[i] = newArreay[i - 1] + newArreay[i - 2];
+    }
+    return newArreay;
+}
+
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+PrintArray(Fibonacci(num));
