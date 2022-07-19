857218086c3d65ed48ca896aaf5e382609c27a7a
igor <igoshinig@yandex.ru>
2022-07-19 13:13:10 +0500

    двумерный массив размером m×n,заполненный случайными целыми числами

--- Seminar_007ДвумерныеМассивы/Task_01/Program.cs
+++ Seminar_007ДвумерныеМассивы/Task_01/Program.cs
@@ -23,7 +23,7 @@ void Print2dArray(int[,] array)
             Console.WriteLine();
         }
 }
-
+/*
 Console.Write("Ведите количство строк: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ведите количство столбцов: ");
@@ -35,3 +35,37 @@ int max = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = CreatRondom2dArray(m,n,min,max);
 Print2dArray(myArray);
+
+*/
+//======================================================
+
+void PrintArray(int[,] matr)                             //массив который заполняет табл. нулями и передает на печать
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)                            //массив заполняет случайными числами и передает на печать
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);            // заполнения случайными значания с 1 до 10
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];          // размер табл. 3 -строки 4 -столбца
+PrintArray(matrix);                     // ввывод на печать
+FillArray(matrix);
+Console.WriteLine();                    // пустая строка
+PrintArray(matrix);                      // ввывод на печать
+
+//=======================================================
