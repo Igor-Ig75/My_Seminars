2627ff5b74a2e5f0f1afb5b6eb4060d59d9d43b6
igor <igoshinig@yandex.ru>
2022-07-22 13:28:19 +0500

    Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.

--- Seminar_009_Рекурсия/Task_01/Program.cs
+++ Seminar_009_Рекурсия/Task_01/Program.cs
@@ -3,11 +3,26 @@
 
 void ShowNums(int n)
 {
-    if(n > 1);
+    if(n > 1)
     {
         ShowNums(n - 1);
     
     }
     Console.Write(n + " ");
 }
-ShowNums(5);
+ShowNums(5);     // Результат 1 2 3 4 5 
+
+
+
+// void ShowNums(int n)
+// {
+//     if(n > 1)
+//     {
+//         Console.Write(n + " ");
+//         ShowNums(n - 1);
+    
+//     }
+//     Console.Write(n + " ");
+// }
+// ShowNums(5);   // Результат 5 4 3 2 1 2 3 4 5 
+
