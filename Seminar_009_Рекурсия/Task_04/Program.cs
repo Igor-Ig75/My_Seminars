096dee1bcfc484098f7ca562e7d99d9aa2d3adef
igor <igoshinig@yandex.ru>
2022-07-23 18:52:19 +0500

    Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.

---
+++ Seminar_009_Рекурсия/Task_04/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает два числа A и B,
+ и возводит число А в целую степень B с помощью рекурсии.*/ 
+
+ 
+int SumOfDigits(int a, int b)
+{
+        if(b > 1)
+            return a * SumOfDigits(a, b - 1);
+        return a;
+}
+
+Console.WriteLine(SumOfDigits(4, 3));
+
+
